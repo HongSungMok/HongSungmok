bbded97ceed0e356290bc3b834253bd59b144853
HongSungMok <hsm153422@gmail.com>
2025-07-25 11:01:27 +0900

    수정사항 기입

--- app.py
+++ app.py
@@ -424,4 +424,5 @@ def fishbot():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render가 제공하는 포트를 사용
+    app.run(host="0.0.0.0", port=port)
